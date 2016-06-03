b18f863aad943c7729b4c86fa95e002ebd9e7c4e
Arctic-Fox <cjurado2@jhu.edu>
2016-06-03 12:20:05 -0700

    bug fix on lead names

--- src/main/java/org/cvrgrid/schiller/SchillerEcgFiles.java
+++ src/main/java/org/cvrgrid/schiller/SchillerEcgFiles.java
@@ -34,10 +34,15 @@ public class SchillerEcgFiles {
 	private static PreprocessReturn preprocess(JAXBContext context, File input) throws JAXBException, IOException {
 		Unmarshaller reader = context.createUnmarshaller();
 		ComXiriuzSemaXmlSchillerEDISchillerEDI comxiriuzsemaxmlschilleredischilleredi = (ComXiriuzSemaXmlSchillerEDISchillerEDI)reader.unmarshal(input);
+
+		return preprocessComplete(comxiriuzsemaxmlschilleredischilleredi);
+	}
+	
+	private static PreprocessReturn preprocessComplete(ComXiriuzSemaXmlSchillerEDISchillerEDI comxiriuzsemaxmlschilleredischilleredi) throws JAXBException, IOException {
 		DecodedLead[] leads = null;
 		PreprocessReturn temp = new PreprocessReturn(comxiriuzsemaxmlschilleredischilleredi, leads, schillerSampRate);
 		
-	    ArrayList<int[]> leadData = new ArrayList<int[]>();
+	    ArrayList<int[]> leadData = new ArrayList<int[]>();	
 	    ArrayList<String> leadNames = null;
 		List<Wavedata> list =  comxiriuzsemaxmlschilleredischilleredi.getEventdata().getEvent().getWavedata();
 		for (Wavedata wd : list) {
@@ -61,34 +66,6 @@ public class SchillerEcgFiles {
 	    }
 		temp.setDecodedLeads(DecodedLead.createFromLeadSet(leadData));
 		temp.setLeadNames(leadNames);
-		return temp;
-	}
-	
-	private static PreprocessReturn preprocessComplete(ComXiriuzSemaXmlSchillerEDISchillerEDI comxiriuzsemaxmlschilleredischilleredi) throws JAXBException, IOException {
-		DecodedLead[] leads = null;
-		PreprocessReturn temp = new PreprocessReturn(comxiriuzsemaxmlschilleredischilleredi, leads, schillerSampRate);
-		
-	    ArrayList<int[]> leadData = new ArrayList<int[]>();	
-	    ArrayList<String> leadNames = null;
-		List<Wavedata> list =  comxiriuzsemaxmlschilleredischilleredi.getEventdata().getEvent().getWavedata();
-		for (Wavedata wd : list) {
-	    	if (wd.getType().equalsIgnoreCase("ecg_rhythms")){
-		    	temp.setPrepSampleRate(wd.getResolution().getSamplerate().getValue());
-		    	List<Channel> channel = wd.getChannel();
-		    	for (Channel subChannel : channel) {
-		    		String channelData = subChannel.getData(); 	
-			    	StringTokenizer tokenizer = new StringTokenizer(channelData, ",");
-		    	    int n = tokenizer.countTokens();
-		    	    int[] list1 = new int[n];
-		    		for (int i = 0; i < n; i++) {
-		    	    	String token = tokenizer.nextToken();
-		    	    	list1[i] = Integer.parseInt(token);
-		    	    }
-	    	    	leadData.add(list1);
-		    	}
-	    	}
-	    }
-		temp.setDecodedLeads(DecodedLead.createFromLeadSet(leadData));
 		return temp;
 	}
 	
