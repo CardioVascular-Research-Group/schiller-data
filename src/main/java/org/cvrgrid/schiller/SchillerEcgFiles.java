ceb953d888d0a598135d6be980b2809100a76a8a
Arctic-Fox <cjurado2@jhu.edu>
2016-06-03 12:04:39 -0700

    Add InputStream handling

--- src/main/java/org/cvrgrid/schiller/SchillerEcgFiles.java
+++ src/main/java/org/cvrgrid/schiller/SchillerEcgFiles.java
@@ -3,6 +3,7 @@ package org.cvrgrid.schiller;
 //import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -20,6 +21,14 @@ import org.cvrgrid.schiller.jaxb.beans.Wavedata;
 public class SchillerEcgFiles {
 	static String schillerSampRate = "";
 	private SchillerEcgFiles() {
+	}
+	
+	private static PreprocessReturn preprocess(JAXBContext context, InputStream inputStream) throws JAXBException, IOException {
+		
+		Unmarshaller reader = context.createUnmarshaller();
+		ComXiriuzSemaXmlSchillerEDISchillerEDI comxiriuzsemaxmlschilleredischilleredi = (ComXiriuzSemaXmlSchillerEDISchillerEDI)reader.unmarshal(inputStream);
+
+		return preprocessComplete(comxiriuzsemaxmlschilleredischilleredi);
 	}
 
 	private static PreprocessReturn preprocess(JAXBContext context, File input) throws JAXBException, IOException {
@@ -53,6 +62,40 @@ public class SchillerEcgFiles {
 		temp.setDecodedLeads(DecodedLead.createFromLeadSet(leadData));
 		temp.setLeadNames(leadNames);
 		return temp;
+	}
+	
+	private static PreprocessReturn preprocessComplete(ComXiriuzSemaXmlSchillerEDISchillerEDI comxiriuzsemaxmlschilleredischilleredi) throws JAXBException, IOException {
+		DecodedLead[] leads = null;
+		PreprocessReturn temp = new PreprocessReturn(comxiriuzsemaxmlschilleredischilleredi, leads, schillerSampRate);
+		
+	    ArrayList<int[]> leadData = new ArrayList<int[]>();	
+	    ArrayList<String> leadNames = null;
+		List<Wavedata> list =  comxiriuzsemaxmlschilleredischilleredi.getEventdata().getEvent().getWavedata();
+		for (Wavedata wd : list) {
+	    	if (wd.getType().equalsIgnoreCase("ecg_rhythms")){
+		    	temp.setPrepSampleRate(wd.getResolution().getSamplerate().getValue());
+		    	List<Channel> channel = wd.getChannel();
+		    	for (Channel subChannel : channel) {
+		    		String channelData = subChannel.getData(); 	
+			    	StringTokenizer tokenizer = new StringTokenizer(channelData, ",");
+		    	    int n = tokenizer.countTokens();
+		    	    int[] list1 = new int[n];
+		    		for (int i = 0; i < n; i++) {
+		    	    	String token = tokenizer.nextToken();
+		    	    	list1[i] = Integer.parseInt(token);
+		    	    }
+	    	    	leadData.add(list1);
+		    	}
+	    	}
+	    }
+		temp.setDecodedLeads(DecodedLead.createFromLeadSet(leadData));
+		return temp;
+	}
+	
+	public static PreprocessReturn preprocess(InputStream inputStream) throws IOException, JAXBException {
+		JAXBContext context = JAXBContext.newInstance("org.cvrgrid.schiller.jaxb.beans");
+		
+		return preprocess(context, inputStream);
 	}
 	
 	public static PreprocessReturn preprocess(File input) throws IOException, JAXBException {
